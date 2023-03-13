17632e75cdc6d130440c8cf88bcde7e38c52fa5e
nguyenducngoc <wangyuhi167@gmail.com>
2023-03-13 18:42:25 +0700

    Update Readme.md

--- phim/models/Users.py
+++ phim/models/Users.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 #Thông tin Users
@@ -11,7 +12,9 @@ class Profile(models.Model):
     address = models.CharField(max_length=100, help_text="Địa chỉ")
     email = models.BooleanField(default=False)
     created_on = models.DateTimeField(default=timezone.now)
+
     def __str__(self):
         return f"{self.user.username}'s Profile"
+    
 
 
