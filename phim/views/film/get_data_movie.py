3a043f48dabbddc072ad4f11415ec5c1da4222ba
nguyenducngoc <wangyuhi167@gmail.com>
2023-03-18 10:21:19 +0700

    Create more crawl data

---
+++ phim/views/film/get_data_movie.py
@@ -0,0 +1,58 @@
+import requests
+from bs4 import BeautifulSoup
+from django.shortcuts import render
+from phim.models.movie_model import Movie
+from django.contrib import messages
+from phim.models.category_model import Category
+import cloudinary
+import cloudinary.uploader
+
+def update_movie1(request):
+    if request.method == 'POST':
+        imdb_url = request.POST.get('imdb_url')
+
+        response = requests.get(imdb_url)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        title = soup.select_one('h1').text
+        title_el = soup.select_one('h2').text
+        description = soup.select_one(' .text > .text-content').text
+        rating = soup.select_one('.featured-attr:contains("IMDB") .text').text
+        contry = soup.select_one('.featured-attr:contains("Quốc gia") .text').text
+        year = soup.select_one('.featured-attr:contains("Năm phát hành") .text').text
+        time = soup.select_one('.featured-attr:contains("Thời lượng") .text').text
+        images = soup.select_one('.media.media-cover')['style']
+        images = soup.select_one('.media-cover')['style'].split('url(')[1].split(')')[0]
+        image_url = cloudinary.uploader.upload(images)
+
+
+        tags = soup.find("div", {"class": "categories"})
+        tag = tags.find_all("a")
+        
+        
+        movie = Movie()
+        movie.title = title
+        movie.description = description
+        movie.title_el = title_el
+        movie.rating = rating
+        movie.contry = contry
+        movie.flyer = image_url['secure_url']
+        movie.year = year
+        movie.time = time
+        
+        movie.save()
+
+        movie.flyer = image_url['secure_url']
+        tag_list = []
+        for tag in soup.select('div.categories a'):
+            a_text = tag.text.strip()
+            tag_list.append(a_text)
+
+        movie.tags.set(tag_list)
+        movie.save()
+
+        messages.success(request, 'Cập nhật phim thành công.')
+
+    return render(request, 'phim/update_movie1.html')
+
